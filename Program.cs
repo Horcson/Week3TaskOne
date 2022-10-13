daebfe3ab88bbf096b7122f491c2ab2fcad8da94
Horcson <ow184@student.aru.ac.uk>
2022-10-13 10:04:57 +0100

    New code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿int num1;
+﻿int[] rainfallamt = new int[12];
+
+rainfallamt[0] = 84;
+rainfallamt[1] = 129;
+rainfallamt[2] = 52;
+rainfallamt[3] = 50;
+rainfallamt[4] = 145;
+rainfallamt[5] = 116;
+rainfallamt[6] = 97;
+rainfallamt[7] = 118;
+rainfallamt[8] = 113;
+rainfallamt[9] = 97;
+rainfallamt[10] = 83;
+rainfallamt[11] = 140;
+
+int rainfallsum = rainfallamt.Sum() / 12;
+
+Console.WriteLine("The average amount of rainfall is " + rainfallsum);
+
+Console.ReadKey();
+
+/*
+int num1;
 int num2;
 string meth;
 int sum;
